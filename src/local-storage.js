97c382d7c97b89f693b8641a471bb76ae3cc6837
MatanB27 <69850880+MatanB27@users.noreply.github.com>
2022-05-20 19:14:25 +0300

    Removed react.strictMode -- LocalStorage working!! NEED TO REFACTOR THE CODE~

--- src/local-storage.js
+++ src/local-storage.js
@@ -1,13 +1,33 @@
-export const getLocalStorage = (key) => {
+export const getLocalStorageItem = (key) => {
     switch(key){
         case 'date':
-            return localStorage.getItem('date');
+            return JSON.parse(localStorage.getItem('date'));
+            
         case 'statistics':
-            return localStorage.getItem('statistics');
+            return JSON.parse(localStorage.getItem('statistics'));
+
+        case 'is_played':
+            return JSON.parse(localStorage.getItem('is_played'));
+
+            case 'win':
+        return JSON.parse(localStorage.getItem('win'));
+
+        case 'lose':
+        return JSON.parse(localStorage.getItem('lose'));
+
+        case 'current_win':
+        return JSON.parse(localStorage.getItem('current_win'));
+
+        case 'current_word':
+        return JSON.parse(localStorage.getItem('prev_word'));
+
+        case 'num_of_guesses':
+        return JSON.parse(localStorage.getItem('num_of_guesses'));
     }
 }
 
 export const addToLocalStorage = (key, payload) => {
+    payload = JSON.stringify(payload);
     localStorage.setItem(key, payload);
 }
 
--- src/screens/game/index.js
+++ src/screens/game/index.js
@@ -6,7 +6,7 @@ import Header from '../../components/header';
 import DrawMan from '../../components/draw-man';
 // import calculateTimeLeft from '../../components/calculateTimeLeft';
 import firebase, {addNewWords, getNewWord} from '../../firebase';
-import {getLocalStorage, addToLocalStorage, isSameDate} from '../../local-storage';
+import {getLocalStorageItem, addToLocalStorage, isSameDate} from '../../local-storage';
 import {handleKeyPress, handleEndGame, handleWinGame} from "../../rules";
 
 function Game(){
@@ -21,33 +21,60 @@ function Game(){
     const [isGameEnd, setGameEnd] = useState(false);
     const [isGameWon, setIsGameWon] = useState(false);
     const [render, setRender] = useState(false);
-
-
-    const [statistics, setStatistics] = useState(
-            {
-                is_played: false,
-                current_win: '',
-                winning: 0,
-                losing: 0,
-            }
-        );
+    const [winAmount, setWinAmount] = useState(0);
+    const [loseAmount, setLoseAmount] = useState(0);
 
     useEffect(() => {
+        getStarted();
+    },[]);
+    
+    const getStarted = () => {
         const today = new Date();
         const currentDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
         
-        const isPlayed = getLocalStorage('statistics')?.is_played;
-        const date = getLocalStorage('date');
+        const isPlayed = getLocalStorageItem('is_played');
+        const localStoageSate = getLocalStorageItem('date');
+        const localStorageWord = getLocalStorageItem('current_word');
+        const localStorageNumOfGuesses = getLocalStorageItem('num_of_guesses');
+        const localStorageWinAmount = getLocalStorageItem('win');
+        const localStorageLoseAmount = getLocalStorageItem('lose');
+        const localStorageIsWon = getLocalStorageItem('current_win');
         
-        if(!isSameDate(currentDate, date)){
-            addToLocalStorage('date', currentDate);
-            if(!isPlayed){
-                addToLocalStorage('statistics', statistics);
+        if(isSameDate(currentDate, localStoageSate)){
+            if(isPlayed){
+                setCurrentWord(localStorageWord);
+                setNumOfGuesses(localStorageNumOfGuesses);
+                setIsGameWon(localStorageIsWon);
+
+                if(localStorageWinAmount){
+                    setWinAmount(localStorageWinAmount);
+                }else{
+                    setWinAmount(0);
+                }
+
+                if(localStorageLoseAmount){
+                    setLoseAmount(localStorageLoseAmount);
+                }else{
+                    setLoseAmount(0);
+                }
+                
+                
+                const currentWin = getLocalStorageItem('current_win')?.current_win;
+                if(currentWin){
+                    setGameEnd(true);
+                    getEndGameTitle('text', true);
+                }else{
+                    setGameEnd(true);
+                    getEndGameTitle('text', false);
+                }
+            }else{
+                getNewWord();
             }
+        }else{
+            getNewWord();
+            addToLocalStorage('date', currentDate);
         }
-        getNewWord();
-    },[]);
-    
+    }
     
     const getNewWord = () => {
         fetch(
@@ -159,11 +186,11 @@ function Game(){
                 <div className='stats-wrapper'>
                     <div className='stats'>
                         <span className='stats-title winning'>Winning</span>
-                        <span className='stats-value'>x</span>
+                        <span className='stats-value'>{winAmount}</span>
                     </div>
                     <div className='stats'>
                         <span className='stats-title losing'>Losing</span>
-                        <span className='stats-value'>y</span>
+                        <span className='stats-value'>{loseAmount}</span>
                     </div>
                 </div>
             </>
@@ -180,14 +207,64 @@ function Game(){
         }
 
         if(isGameWon){
+            handleGameEnd(isGameWon);
             return(
                 getEndGameTitle(`Congratulations, you won the game after ${numOfGuesses} guesses.`, isGameWon)
             )
         }else if(isGameEnd){
+            handleGameEnd(isGameWon);
             return(
                 getEndGameTitle(`Game over! you reached ${numOfGuesses} guesses and lost.`, isGameWon)
             )
         }
+    }
+
+    const handleGameEnd = (isWon) => {
+        addToLocalStorage('prev_word', currentWord);
+        addToLocalStorage('num_of_guesses', numOfGuesses);
+        const is_played = getLocalStorageItem('is_played');
+        console.log('win', winAmount);
+        console.log('lose', loseAmount);
+        if(!is_played){
+            let win = getLocalStorageItem('win');
+            let lose = getLocalStorageItem('lose');
+            if(isWon){
+                addToLocalStorage('current_win', true);
+                
+
+                if(!win){
+                    addToLocalStorage('win', 1);
+                    setWinAmount(1);
+                }else{
+                    addToLocalStorage('win', ++win);
+                    setWinAmount(win);
+                }
+                if(!lose){
+                    setLoseAmount(0);
+                }else{
+                    setLoseAmount(lose);
+                }
+        
+            }else{
+                addToLocalStorage('current_win', false);
+                
+                if(!lose){
+                    addToLocalStorage('lose', 1);
+                    setLoseAmount(1);
+                }else{
+                    addToLocalStorage('lose', ++lose);
+                    setLoseAmount(lose);
+                }
+
+                if(!win){
+                    setWinAmount(0);
+                }else{
+                setWinAmount(win)
+                }
+            }
+        }
+        
+        addToLocalStorage('is_played', true);
     }
 
     return(
