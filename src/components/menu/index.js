97c1ef7d00f38c5ce86d727917508c4b05c1e0f6
MatanB27 <69850880+MatanB27@users.noreply.github.com>
2022-06-24 21:54:29 +0300

    added aboutus text

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -9,24 +9,27 @@ function Menu (props) {
         setIsMenuOpen,
     } = props;
 
-
     return(
-        <div className={"menu-wrapper " + (isMenuOpen ? 'active' : '')}>
-            <div className={"menu-header-wrapper"}>
-                <div className={"menu-header-inner"}>
-                    <span className={"header-title"}>Menu</span>
-                    <span  onClick={() => setIsMenuOpen(!isMenuOpen)} className={"close-x"}>X</span>
+        <>
+        <div className={"black-screen " + (isMenuOpen ? 'active' : '')}
+            onClick={() => {setIsMenuOpen(!isMenuOpen)}}></div>
+            <div className={"menu-wrapper " + (isMenuOpen ? 'active' : '')}>
+                <div className={"menu-header-wrapper"}>
+                    <div className={"menu-header-inner"}>
+                        <span className={"header-title"}>Menu</span>
+                        <span  onClick={() => setIsMenuOpen(!isMenuOpen)} className={"close-x"}>X</span>
+                    </div>
+                </div>
+                <div className="menu">
+                    <div className="routes-wrapper">
+                        <NavLink className='link' activeclassname='active' 
+                            to='/'>Fuzzle</NavLink>
+                        <NavLink className='link' activeclassname='active' 
+                            to='/aboutus'>About us</NavLink>
+                    </div>
                 </div>
             </div>
-            <div className="menu">
-                <div className="routes-wrapper">
-                    <NavLink class='link' activeClass='active' 
-                        to='/'>Fuzzle</NavLink>
-                    <NavLink class='link' activeClass='active' 
-                        to='/aboutus'>About us</NavLink>
-                </div>
-            </div>
-        </div>
+        </>
     );
 }
 
--- src/screens/aboutus/index.js
+++ src/screens/aboutus/index.js
@@ -6,6 +6,23 @@ function AboutUs(){
     return(
         <div className="about-us-wrapper">
             <Header/>
+
+            <div className="text-wrapper">
+                <span className="text">
+                Who we are?
+                <br></br><br></br>
+                Fuzzle Team is proud to present the site fuzzle.online! The purpose of the site is to give the user a challenging game experience in different and varied games.
+                <br></br><br></br>
+                In each update we will add more and more games.
+                <br></br><br></br>
+                Our ambition is to grow and develop and become a site for particularly challenging thinking games.
+
+                <br></br><br></br><br></br><br></br>
+                
+                We are excited to know what the future holds for us, and where the site will take the creativity we have built.
+                There is something to look forward to... :)
+                </span>
+            </div>
         </div>
     )
 }
